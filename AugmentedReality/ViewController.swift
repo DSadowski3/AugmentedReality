efb8746cc1f2c315605b15960de9f1b43d7ceac4
DSadowski3 <dsadowski2001@dominiks-macbook-pro.local>
2017-01-25 20:41:53 -0600

    Initial Commit

---
+++ AugmentedReality/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AugmentedReality
+//
+//  Created by Dominik Sadowski on 1/25/17.
+//  Copyright © 2017 Dominik Sadowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
